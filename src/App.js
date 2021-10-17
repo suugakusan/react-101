c0d0d929b6216955ac7dd4787c4f7c303c44277f
tomohiro.kojima <niniseseko@gmail.com>
2021-10-17 03:42:15 +0000

    複数のusestate

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0)
-  const countUp = () => setCount(count + 1)
-  const countDown = () => setCount(count - 1)
-  const countUp2 = () => setCount(precount => precount + 1)
-  const countReset = () => setCount(0)
-  const count2 = () => setCount(count * 2)
-  const count3 = () => setCount(precount => {
-    return precount % 3 === 0 ? precount / 3 : precount })
+const App = props => {
   
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+  
+  const reset = () =>{
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
     <>
-      <div>count:{count}</div>
-      <div>
-        <button onClick={countUp}>+1</button>
-        <button onClick={countDown}>-1</button>
-      </div>
-      <div>
-        <button onClick={countUp2}>+1</button>
-      </div>
-      <button onClick={countReset}>Reset</button>
-      <button onClick={count2}>×2</button>
-      <button onClick={count3}>3</button>
+      <p>現在の{name}は、{price}円</p>
+      <button onClick={()=>setPrice(price+1)}>+1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={e=>setName(e.target.value)}/>
     </>
   );
 }
 
+App.defaultProps = {
+    name: "",
+    price: 1000
+}
 export default App;
