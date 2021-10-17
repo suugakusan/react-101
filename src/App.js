e0e8837da05559c969d60ef126d04427a03ef7d1
tomohiro.kojima <niniseseko@gmail.com>
2021-10-17 03:05:43 +0000

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const countUp = () => setCount(count + 1)
+  const countDown = () => setCount(count - 1)
+  const countUp2 = () => setCount(precount => precount + 1)
+  const countReset = () => setCount(0)
+  const count2 = () => setCount(count * 2)
+  const count3 = () => setCount(precount => {
+    return precount % 3 === 0 ? precount / 3 : precount })
+  
   return (
-    <div>
-      テンプレート
-    </div>
+    <>
+      <div>count:{count}</div>
+      <div>
+        <button onClick={countUp}>+1</button>
+        <button onClick={countDown}>-1</button>
+      </div>
+      <div>
+        <button onClick={countUp2}>+1</button>
+      </div>
+      <button onClick={countReset}>Reset</button>
+      <button onClick={count2}>×2</button>
+      <button onClick={count3}>3</button>
+    </>
   );
 }
 
