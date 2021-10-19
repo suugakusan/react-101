1ce404180847e10a8d49d6fc1cbaa287b87d6c11
tomohiro.kojima <niniseseko@gmail.com>
2021-10-19 20:57:25 +0000

    コンポーネント化

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,18 @@
-import React, {useReducer, useState} from 'react';
+import React, { useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import reducer from '../reducers'
 import EventForm from './EventForm'
-import Event from './Event'
+
+import Events from './Events'
+import reducer from '../reducers'
 const App = () => {
   const [state, dispatch] = useReducer( reducer, [])
   
   return (
-    <>
-    <EventForm />
       <div className = 'container-fluid'>
-        <h4>イベント一覧</h4>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>タイトル</th>
-              <th>本文</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
-          </tbody>
-        </table>
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
       </div>
-    </>
   );
 }
 
-
-export default App;
+export default App
